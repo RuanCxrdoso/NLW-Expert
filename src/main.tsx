c32f22e45b6d1d7a1d6e47b37a1e990651d7e98b
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-02-15 15:46:37 -0300

    fix: Vercel bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './app.tsx'
+import { App } from './App.tsx'
 import { Toaster } from 'sonner'
 import './index.css'
 
