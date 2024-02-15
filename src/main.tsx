087bed1e0f4056e77428df7fc3fbd0effab78080
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-02-15 15:44:03 -0300

    feat: image preview

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './app'
+import { App } from './app.tsx'
 import { Toaster } from 'sonner'
 import './index.css'
 
